c934441f658ae8f28aa2df534f95c32943c288f8
Vladislav <vladislaw.sirotkin@yandex.ru>
2026-05-16 16:50:34 +0300

    Добавлены лайки, друзья

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,75 +1,65 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
-    private static final LocalDate MIN_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+    private final FilmService filmService;
 
-    private final Map<Long, Film> films = new HashMap<>();
+    @GetMapping
+    public Collection<Film> findAllFilms() {
+        log.info("GET /films - получение всех фильмов");
+        return filmService.findAllFilms();//
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable @Positive Long id) {
+        log.info("GET /films/{} - получение фильма по ID", id);
+        return filmService.getFilmById(id);
+    }
 
     @PostMapping
-    public Film create(@Valid @RequestBody Film film) {
-        validateFilm(film);
-        log.info("Получен запрос на добавление фильма");
-
-        film.setId(getNextId());
-
-        films.put(film.getId(), film);
-        log.info("Добавлен фильм {}", film.getId());
-        return film;
-    }
-
-    private long getNextId() {
-        long currentMaxId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
-    }
-
-    private void validateFilm(Film film) {
-        if (film.getReleaseDate().isBefore(MIN_RELEASE_DATE)) {
-            log.error("Указана дата релиза фильма ранее 28.12.1895");
-            throw new ValidationException("Дата релиза не может быть ранее 28.12.1895");
-        }
+    public Film createFilm(@Valid @RequestBody Film film) {
+        log.info("POST /films - создание фильма: {}", film.getName());
+        return filmService.createFilm(film);
     }
 
     @PutMapping
-    public Film updateFilm(@Valid @RequestBody Film newFilm) {
-
-        log.info("Получен запрос на изменение фильма");
-
-        if (newFilm.getId() == null) {
-            log.error("Не указан ID фильма");
-            throw new ValidationException("Id должен быть указан");
-        }
-
-        if (!films.containsKey(newFilm.getId())) {
-            log.error("Указан не существующий айди");
-            throw new ValidationException("Фильм с таким id не существует");
-        }
-
-        validateFilm(newFilm);
-        films.put(newFilm.getId(), newFilm);
-        log.info("Изменен фильм {}", newFilm.getId());
-        return newFilm;
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        log.info("PUT /films - обновление фильма с ID: {}", film.getId());
+        return filmService.updateFilm(film);
     }
 
-    @GetMapping
-    public Collection<Film> findAlliFilms() {
-        return films.values();
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(
+            @PathVariable @Positive Long id,
+            @PathVariable @Positive Long userId) {
+        log.info("PUT /films/{}/like/{} - добавление лайка", id, userId);
+        filmService.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(
+            @PathVariable @Positive Long id,
+            @PathVariable @Positive Long userId) {
+        log.info("DELETE /films/{}/like/{} - удаление лайка", id, userId);
+        filmService.deleteLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10") @Positive Integer count) {
+        log.info("GET /films/popular?count={} - получение популярных фильмов", count);
+        return filmService.getPopularFilms(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,84 +1,78 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
-    private final Map<Long, User> users = new HashMap<>();
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable @Positive Long id) {
+        log.info("GET /users/{} - получение пользователя по ID", id);
+        return userService.getUserById(id);
+    }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
-
-        log.info("Получен запрос на создание пользователя");
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-            log.info("Для пользователя с логином {} установлено имя из логина", user.getLogin());
-        }
-
-        user.setId(getNextId());
-        log.info("Добавлен пользователь{}", user.getId());
-
-        users.put(user.getId(), user);
-        return user;
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
+        log.info("POST /users - создание пользователя: {}", user.getLogin());
+        return userService.createUser(user);
     }
 
     @PutMapping
-    public User updateUser(@Valid @RequestBody User newUser) {
+    public User updateUser(@Valid @RequestBody User user) {
+        log.info("PUT /users - обновление пользователя с ID: {}", user.getId());
+        return userService.updateUser(user);
+    }
 
-        log.info("Получен запрос на изменение пользователя");
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(
+            @PathVariable @Positive Long id,
+            @PathVariable @Positive Long friendId) {
+        log.info("PUT /users/{}/friends/{} - добавление в друзья", id, friendId);
+        userService.addFriend(id, friendId);
+    }
 
-        User oldUser = users.get(newUser.getId());
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void removeFriend(
+            @PathVariable @Positive Long id,
+            @PathVariable @Positive Long friendId) {
+        log.info("DELETE /users/{}/friends/{} - удаление из друзей", id, friendId);
+        userService.removeFriend(id, friendId);
+    }
 
-        if (newUser.getId() == null) {
-            log.error("Не указан id пользователя");
-            throw new ValidationException("Id должен быть указан");
-        }
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable @Positive Long id) {
+        log.info("GET /users/{}/friends - получение списка друзей", id);
+        return userService.getFriends(id);
+    }
 
-        if (!users.containsKey(newUser.getId())) {
-            log.error("Указан несуществующий id");
-            throw new ValidationException("Пользователь с id = " + newUser.getId() + " не найден");
-        }
-
-        if (!newUser.getEmail().equals(oldUser.getEmail())) {
-            boolean emailExists = users.values().stream()
-                    .anyMatch(u -> u.getEmail().equals(newUser.getEmail()));
-            if (emailExists) {
-                log.error("Указанный Email занят");
-                throw new ValidationException("Этот Email уже занят");
-            }
-        }
-
-        if (newUser.getName() == null || newUser.getName().isBlank()) {
-            newUser.setName(newUser.getLogin());
-        }
-
-        users.put(newUser.getId(), newUser);
-        return newUser;
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(
+            @PathVariable @Positive Long id,
+            @PathVariable @Positive Long otherId) {
+        log.info("GET /users/{}/friends/common/{} - поиск общих друзей", id, otherId);
+        return userService.getCommonFriends(id, otherId);
     }
 
     @GetMapping
-    public Collection<User> findAllUsers() {
-        return users.values();
+    public Collection<User> getAllUsers() {
+        log.info("GET /users - получение всех пользователей");
+        return userService.findAllUsers();
     }
 }
